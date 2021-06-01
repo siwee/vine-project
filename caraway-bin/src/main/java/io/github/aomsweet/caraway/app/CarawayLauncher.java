6816012109b7bce08985c4d0fa8330cb62315660
aomsweet <aomsweet@tutanota.com>
2021-06-01 15:50:31 +0800

    add start log
    Signed-off-by: aomsweet <aomsweet@tutanota.com>

--- caraway-bin/src/main/java/io/github/aomsweet/caraway/app/CarawayLauncher.java
+++ caraway-bin/src/main/java/io/github/aomsweet/caraway/app/CarawayLauncher.java
@@ -2,16 +2,25 @@ package io.github.aomsweet.caraway.app;
 
 import ch.qos.logback.classic.LoggerContext;
 import io.github.aomsweet.caraway.CarawayBootstrap;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 
 /**
  * @author aomsweet
  */
 public class CarawayLauncher {
 
-    static LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+    private static final LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+    private static final Logger logger = LoggerFactory.getLogger(CarawayLauncher.class);
 
     public static void main(String[] args) {
+        RuntimeMXBean mx = ManagementFactory.getRuntimeMXBean();
+        String name = mx.getName();
+        logger.info("Starting {} on {} with process id {} ({})", CarawayLauncher.class.getSimpleName(),
+            name.substring(name.indexOf('@') + 1), mx.getPid(), System.getProperty("user.dir"));
         CarawayBootstrap bootstrap = new CarawayBootstrap()
             .withPort(2228);
         bootstrap.start();
