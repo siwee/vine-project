b2c534dbeeb40b562916bc78ad182bf9a6c4ab25
aomsweet <aomsweet@tutanota.com>
2021-06-30 23:10:47 +0800

    Log loading in developer mode.
    Signed-off-by: aomsweet <aomsweet@tutanota.com>

--- caraway-bin/src/main/java/io/github/aomsweet/caraway/app/logback/LogbackContext.java
+++ caraway-bin/src/main/java/io/github/aomsweet/caraway/app/logback/LogbackContext.java
@@ -1,11 +1,14 @@
 package io.github.aomsweet.caraway.app.logback;
 
+import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.PatternLayout;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.encoder.LayoutWrappingEncoder;
 import org.slf4j.LoggerFactory;
+
+import java.net.URL;
 
 /**
  * @author aomsweet
@@ -16,13 +19,17 @@ public class LogbackContext {
 
     public LogbackContext() {
         this.logbackContext = (LoggerContext) LoggerFactory.getILoggerFactory();
-        configure();
+        configure(isDevMode());
     }
 
-    public void configure() {
+    public void configure(boolean isDevMode) {
+        if (isDevMode) {
+            return;
+        }
         logbackContext.stop();
 
         ConsoleAppender<ILoggingEvent> consoleAppender = new ConsoleAppender<>();
+        consoleAppender.setWithJansi(true);
         consoleAppender.setContext(logbackContext);
         consoleAppender.setName("console");
 
@@ -42,12 +49,18 @@ public class LogbackContext {
         consoleAppender.start();
 
         Logger rootLogger = logbackContext.getLogger(Logger.ROOT_LOGGER_NAME);
+        rootLogger.setLevel(Level.INFO);
         rootLogger.addAppender(consoleAppender);
 
         logbackContext.setPackagingDataEnabled(true);
         if (!logbackContext.isStarted()) {
             logbackContext.start();
         }
+    }
+
+    public boolean isDevMode() {
+        URL url = this.getClass().getClassLoader().getResource("logback-test.xml");
+        return url != null;
     }
 
     public void stop() {
