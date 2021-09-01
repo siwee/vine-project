5f963fba6765e62b2c828a3cc29f338ae17d9012
aomsweet <aomsweet@tutanota.com>
2021-09-01 18:01:05 +0800

    Cert generation.
    Signed-off-by: aomsweet <aomsweet@tutanota.com>

---
+++ cyber-bouncycastle/src/test/java/io/github/aomsweet/cyber/utils/GenCert.java
@@ -0,0 +1,83 @@
+package io.github.aomsweet.cyber.utils;
+
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.BasicConstraints;
+import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.FileTime;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.Date;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * @author aomsweet
+ */
+public class GenCert {
+
+    public static void main(String[] args) throws Exception {
+        GenCert gen = new GenCert();
+        KeyPair keyPair = gen.genKeyPair(1024);
+        String subject = "O=https://github.com/aomsweet, OU=Cyber CA, CN=Cyber ROOT CA";
+        LocalDate caNotBefore = LocalDate.of(2021, 9, 1);
+        LocalDate caNotAfter = caNotBefore.plusYears(100);
+        X509Certificate cert = gen.genCert(subject, subject,
+            Date.from(caNotBefore.atStartOfDay().toInstant(ZoneOffset.UTC)),
+            Date.from(caNotAfter.atStartOfDay().toInstant(ZoneOffset.UTC)),
+            keyPair);
+
+        String rootDir = System.getProperty("user.dir");
+        String derPath = "cyber-core/src/main/resources/io/github/aomsweet/cyber/http/mitm/cert/CYBER_CERT.DER";
+        String keyPath = "cyber-core/src/main/resources/io/github/aomsweet/cyber/http/mitm/cert/PRIVATE.KEY";
+        gen.toFile(cert.getEncoded(), new File(rootDir, derPath), caNotBefore);
+        gen.toFile(keyPair.getPrivate().getEncoded(), new File(rootDir, keyPath), caNotBefore);
+    }
+
+    public KeyPair genKeyPair(int size) throws NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+        keyPairGen.initialize(size, new SecureRandom());
+        return keyPairGen.genKeyPair();
+    }
+
+    public X509Certificate genCert(String issuer, String subject, Date caNotBefore, Date caNotAfter, KeyPair keyPair) throws Exception {
+        JcaX509v3CertificateBuilder jv3Builder = new JcaX509v3CertificateBuilder(new X500Name(issuer),
+            BigInteger.valueOf(ThreadLocalRandom.current().nextLong(Integer.MAX_VALUE, Long.MAX_VALUE)),
+            caNotBefore,
+            caNotAfter,
+            new X500Name(subject),
+            keyPair.getPublic());
+        jv3Builder.addExtension(Extension.basicConstraints, true, new BasicConstraints(0));
+        ContentSigner signer = new JcaContentSignerBuilder("SHA256WithRSAEncryption")
+            .build(keyPair.getPrivate());
+        return new JcaX509CertificateConverter().getCertificate(jv3Builder.build(signer));
+    }
+
+    public void toFile(byte[] bytes, File file, LocalDate date) throws IOException {
+        File parent = file.getParentFile();
+        if (!parent.exists()) {
+            parent.mkdirs();
+        }
+        Path path = file.toPath();
+        Files.write(file.toPath(), bytes);
+
+        FileTime fileTime = FileTime.from(date.atStartOfDay().toInstant(ZoneOffset.UTC));
+        BasicFileAttributeView view = Files.getFileAttributeView(path, BasicFileAttributeView.class);
+        view.setTimes(fileTime, fileTime, fileTime);
+    }
+
+}
